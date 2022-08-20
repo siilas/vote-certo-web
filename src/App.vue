64583b3233fbf1cb55f126a063b4e7cc07e2f751
Deny Barbosa <deny.stanley.06@gmail.com>
2022-08-20 10:21:12 -0300

    footer primordial

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+</script>
+
+<template>
+
+  <Header></Header>
+  <RouterView />
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+        <span>Direitos Reservados @2022</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+
+.footer {
+    text-align: center;
+    width: 100%;
+}
+
+</style>
